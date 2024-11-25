c38be1b30adc2ec8579c8ef9da688d4ae95c730f
Seok Hyun Kim <genishk@Seoks-MBP.lan>
2024-11-25 15:31:16 -0500

    Add model and training script files

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+UFC Data Analysis Package
+
+This package provides functionality for analyzing UFC fight data.
+Contains modules for data processing, analysis, and visualization.
+"""
+
+__version__ = '0.1.0'
+__author__ = 'UFC Data Analysis Team'
+
+from . import data_processing
+
+# List of modules that should be imported when using "from package import *"
+__all__ = ['data_processing']
